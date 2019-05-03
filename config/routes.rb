b20ce7528fe5621b2a7c609d8e59cb76c656d6e8
Douglas <dougmarques10@gmail.com>
2019-05-03 00:08:59 -0300

    create base controllers and mailer

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,14 @@
 require 'sidekiq/web'
 
 Rails.application.routes.draw do
+  get 'members/create'
+
+  get 'members/destroy'
+
+  get 'members/update'
+
+  get 'pages/home'
+
   devise_for :users, :controllers => { registrations: 'registrations' }
   mount Sidekiq::Web => '/sidekiq'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
